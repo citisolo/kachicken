a91de31fb46b98442815ba5e3934032407bf4af4
citisolo@gmail.com <citisolo@gmail.com>
2018-03-09 18:42:54 +0000

    minor changes to make work accross platform

--- client/src/Components/SubComponents/MenuPlanner.js
+++ client/src/Components/SubComponents/MenuPlanner.js
@@ -71,7 +71,7 @@ const customStyles = {
     marginRight           : '-50%',
     transform             : 'translate(-50%, -50%)',
     width                 : '800px',
-    height                : '900px'
+    height                : '500px'
   }
 };
 
